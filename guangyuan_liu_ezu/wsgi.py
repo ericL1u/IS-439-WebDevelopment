ffa8675a071f5457233732377c99f57efd1fb7a9
ericL1u <rxyhliu@gmail.com>
2022-04-24 22:15:11 -0500

    init

---
+++ guangyuan_liu_ezu/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for guangyuan_liu_ezu project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guangyuan_liu_ezu.settings')
+
+application = get_wsgi_application()
